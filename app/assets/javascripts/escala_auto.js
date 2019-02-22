689f5b9cf5b608d09671107e1d4e65750d805804
colmexdev <github@colmex.mx>
2019-02-22 12:40:09 -0600

    Página inicial

--- app/assets/javascripts/escala_auto.js
+++ app/assets/javascripts/escala_auto.js
@@ -13,3 +13,17 @@ window.onresize = function(){
 $(window).on("orientationchange",function(){
 	$(window).trigger("resize");
 });
+
+function escalarSlider(arr_a){
+	var ancho_slide = $("#div_slider").width();
+	var alto_slide = ancho_slide*(17/40);
+	$('#div_slider').css({'height':alto_slide+'px'});
+	$('.ghost-slider').css("height",alto_slide+"px");
+}
+
+function reajustarBadges(arr){
+	for( var i = 0; i < gon.cant_sliders; i++){
+		var ancho_nuevo = ($("#div_slider").width() * arr[i]) / 1341;
+		$( "#badge-" + i ).css({"width": ancho_nuevo + "px", "max-width": arr[i]+"px" });
+	}
+}
