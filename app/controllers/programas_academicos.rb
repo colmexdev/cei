4e0b6296a5d0f4e8065b38584657705b13e04427
colmexdev <github@colmex.mx>
2019-03-05 14:53:42 -0600

    Contacto

--- app/controllers/programas_academicos.rb
+++ app/controllers/programas_academicos.rb
@@ -3,9 +3,6 @@ class ProgramasAcademicosController < ApplicationController
   def programas
     @titulo = (request.fullpath.include?("politica-administracion-publica") ? "pap" : (request.fullpath.include?("relaciones-internacionales") ? "ri" : "cp"))
     @grado = (request.fullpath.include?("ciencia-politica") ? "mt" : "lic")
-    respond_to do |format|
-      format.html render: 'programas'
-    end
   end
 
 end
