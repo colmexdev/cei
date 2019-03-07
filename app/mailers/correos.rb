9a9c16cf72e202ea0f0aeff787d876a4ec68e1e0
colmexdev <tenoriole1993@gmail.com>
2019-03-07 02:55:45 -0600

    Contacto

--- app/mailers/correos.rb
+++ app/mailers/correos.rb
@@ -1,10 +1,10 @@
 class CorreosMailer < ActionMailer::Base
   default to: 'tenoriole1993@gmail.com'
 
-  def contacto_mail
-    @nombre = params[:nombre]
-    @correo = params[:correo]
-    @asunto = params[:cuerpo]
+  def contacto_mail(nombre,correo,cuerpo)
+    @nombre = nombre
+    @correo = correo
+    @asunto = cuerpo
     mail(from: @correo, subject: 'Duda o comentario')
   end
 end
