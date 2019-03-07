25f9652180cc7cda339a8df4d8db5466962bbb50
colmexdev <tenoriole1993@gmail.com>
2019-03-07 05:34:27 -0600

    Contacto

--- app/mailers/correos.rb
+++ app/mailers/correos.rb
@@ -1,10 +1,9 @@
 class CorreosMailer < ActionMailer::Base
-  default to: 'etenorio@colmex.mx'
 
   def contacto_mail(nombre,correo,cuerpo)
     @nombre = nombre
     @correo = correo
     @cuerpo = cuerpo
-    mail(from: 'etenorio@colmex.mx', correo: @correo, subject: 'Duda o comentario')
+    mail(from: 'etenorio@colmex.mx', to: @correo, subject: 'Duda o comentario')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'hubnlb.colmex.mx',
-    port:                 25,
-    domain:               'colmex.mx',
-    enable_starttls_auto: true}
+    port:                 25}
   #config.action_mailer.delivery_method = :sendmail
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = true
