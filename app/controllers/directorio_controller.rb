ae6d7f9c791bcabeb80f47367e0ccd4c9a8ad27d
colmexdev <tenoriole1993@gmail.com>
2019-03-08 23:09:26 -0600

    Directorio

--- app/controllers/directorio_controller.rb
+++ app/controllers/directorio_controller.rb
@@ -4,7 +4,6 @@ class DirectorioController < ApplicationController
     @titulo = (request.fullpath.include?("academico") ? "academico" : "administrativo")
     @t_a = (request.fullpath.include?("academico") ? "temas" : "area")
     @p_s = (request.fullpath.include?("academico") ? "publicaciones" : "servicios")
-    logger.debug request.GET
     respond_to do |format|
       format.html
       format.js
