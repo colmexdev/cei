e7f05726608fc0a6494ea37f35d66587366060ee
colmexdev <tenoriole1993@gmail.com>
2019-03-09 02:48:06 -0600

    Directorio

--- app/controllers/directorio_controller.rb
+++ app/controllers/directorio_controller.rb
@@ -4,10 +4,11 @@ class DirectorioController < ApplicationController
     @titulo = (request.fullpath.include?("academico") ? "academico" : "administrativo")
     @t_a = (request.fullpath.include?("academico") ? "temas" : "area")
     @p_s = (request.fullpath.include?("academico") ? "publicaciones" : "servicios")
+    @total = params[:total] || 0
     respond_to do |format|
       format.html
       format.js
-      format.json {render json: {total: params[:total] || 0}}
+      format.json {render json: {total: @total}}
     end
   end
 
