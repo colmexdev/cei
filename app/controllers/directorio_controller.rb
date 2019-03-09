b8bb46a9b646973e063d04011d646dba50472c69
colmexdev <tenoriole1993@gmail.com>
2019-03-08 23:01:00 -0600

    Directorio

--- app/controllers/directorio_controller.rb
+++ app/controllers/directorio_controller.rb
@@ -4,6 +4,7 @@ class DirectorioController < ApplicationController
     @titulo = (request.fullpath.include?("academico") ? "academico" : "administrativo")
     @t_a = (request.fullpath.include?("academico") ? "temas" : "area")
     @p_s = (request.fullpath.include?("academico") ? "publicaciones" : "servicios")
+    logger.debug request.GET
     respond_to do |format|
       format.html
       format.js
--- app/controllers/directorio_controller.rb~
+++ app/controllers/directorio_controller.rb~
@@ -4,6 +4,10 @@ class DirectorioController < ApplicationController
     @titulo = (request.fullpath.include?("academico") ? "academico" : "administrativo")
     @t_a = (request.fullpath.include?("academico") ? "temas" : "area")
     @p_s = (request.fullpath.include?("academico") ? "publicaciones" : "servicios")
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
