4e46993e60a0ffaf9814e6b2d61c27ceb29341b3
colmexdev <github@colmex.mx>
2019-03-20 17:53:46 -0600

    Cursos y portadas de revistas

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -3,7 +3,7 @@ class PrincipalController < ApplicationController
   def index
     @fi = HTTParty.get('https://forointernacional.colmex.mx/index.php/fi', verify: false)
     @img_foro = Nokogiri::HTML(@fi).search("#homepageImage").children.attribute("src").value
-    @agora = HTTPary.get('https://agora.colmex.mx/', verify: false)
+    @agora = HTTParty.get('https://agora.colmex.mx/', verify: false)
     @img_agora = "https://agora.colmex.mx" + Nokogiri::HTML(@agora).at('img[alt="DISPONIBLE AHORA"]').attribute("src").value
   end
 
