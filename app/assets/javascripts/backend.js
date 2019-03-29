09c25de19c95d246db6f82d81dd44449095e284f
colmexdev <github@colmex.mx>
2019-03-29 13:37:33 -0600

    Panel

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -183,7 +183,7 @@ function highlightSort(){
 	var fo;
 	for(var i = 0; i < f_fields.length; i++){
 		fo = decodeURIComponent(f_fields[i]).split("*");
-		document.querySelector("[data-tipo=filtro][data-field=" + fo[0] + "][data-op=" + fo[1] + "]").value = decodeURIComponent(f_vals[i].replace(/\\\\/g,"\\");
+		document.querySelector("[data-tipo=filtro][data-field=" + fo[0] + "][data-op=" + fo[1] + "]").value = decodeURIComponent(f_vals[i].replace(/\\\\/g,"\\"));
 	}
 }
 
