a40b5f2004a185658c2125a7cf97335bc3fd98d0
colmexdev <github@colmex.mx>
2019-03-29 17:24:25 -0600

    Panel

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -48,8 +48,8 @@ class PanelController < ApplicationController
       campos = params[:filt_fo].map {|x| CGI.unescape(x.split("*")[0]) }
       ops = params[:filt_fo].map {|x| operadores[CGI.unescape(x.split("*")[1]).to_sym] }
       vals = params[:filt_v].map {|x| CGI.unescape(x).gsub("'","''") }
-      @filter_query = campos.zip(ops,vals).map {|a| (@models.columns_hash[a[0]].type == :text ? ("lower(" + a[0] + ")") : a[0]) + a[1] + (a[1] == " like " ? ("'%" + a[2].downcase.gsub(/%/,'\\\\%').gsub(/_/,'\\\\_') + "%'").gsub("\\","\\\\") : (a[1] == " = " ? ("'" + a[2].downcase.gsub("\\","\\\\") + "'") : (@models.columns_hash[a[0]].type == :date ? ("to_date('" + a[2] + "','YYYY-MM-DD')") : a[2]))) }.join(" AND ")
-      logger.debug vals.map{|a| a.gsub("\\","\\\\").gsub(/%/,'\\%').gsub(/_/,'\\_')}
+      @filter_query = campos.zip(ops,vals).map {|a| (@models.columns_hash[a[0]].type == :text ? ("lower(" + a[0] + ")") : a[0]) + a[1] + (a[1] == " like " ? ("'%" + a[2].downcase.gsub(/%/,'\\\\%').gsub(/_/,'\\\\_') + "%'").gsub("\\","\\\\\\") : (a[1] == " = " ? ("'" + a[2].downcase.gsub("\\","\\\\") + "'") : (@models.columns_hash[a[0]].type == :date ? ("to_date('" + a[2] + "','YYYY-MM-DD')") : a[2]))) }.join(" AND ")
+      logger.debug vals.map{|a| a.gsub("\\","\\\\\\").gsub(/%/,'\\%').gsub(/_/,'\\_')}
     end
     if params[:keyword].present?
       query
