c67d450c89b38e46576eaa4b523407daea07d688
colmexdev <github@colmex.mx>
2019-04-11 16:33:26 -0500

    Error

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ desc 'Generate error page'
 task :generate_500_html do
   on roles(:web) do |host|
     public_500_html = File.join(release_path, "public/500.html")
-    execute :curl, "-k", "https://#{host.hostname}/500", "> #{public_500_html}"
+    execute :curl, "-k", "https://ceitest.colmex.mx/500", "> #{public_500_html}"
   end
 end
 
